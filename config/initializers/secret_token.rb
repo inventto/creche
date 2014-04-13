d3b844fb3a34a5094f651559ffbf50d8e1822518
Jônatas Paganini <jonatasdp@gmail.com>
2014-04-13 10:03:23 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Creche::Application.config.secret_key_base = '89642dc86d8def1910cf0375193e8c3086eef80a26175786f384e50c8ad3c9b79e2d5c3d0a613b5d1291e68fa3a5e31108794db78fef80a706493894268c8d19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Creche::Application.config.session_store :cookie_store, key: '_creche_session'
