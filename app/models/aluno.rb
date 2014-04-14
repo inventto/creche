453b456d229d6c30b3e6bc25161c595111488fe6
Jônatas Paganini <jonatasdp@gmail.com>
2014-04-13 21:44:59 -0300

    improving links and translation

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,11 +1,17 @@
 class Aluno < ActiveRecord::Base
   validates_presence_of :nome
   has_many :diarios
+  has_many :matriculas
+  has_many :turmas, through: :matriculas
+
 
   def hoje! em, agiu, como
     diario = diarios.find_or_initialize_by(em: em, agiu: agiu)
     diario.como = como
     diario.save
+  end
+  def turma
+    turmas.last(order: "ano_letivo desc")
   end
   def to_s
     nome
